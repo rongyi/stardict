d7d085d6ad29ded2f0ac3c487e9dcfc47c853add
rongyi <sqrongyi@163.com>
2017-11-28 16:56:48 +0800

    Add comment

--- cmd/withdict/main.go
+++ cmd/withdict/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"log"
 	"bytes"
+	"log"
 	"stardict"
 )
 
 func main() {
+	// using this tool to convert data to golang source code
+	// https://github.com/jteeuwen/go-bindata
+	// the dict I'm using is contained in go source code with command:
+	// go-bindata -o dict.go ifo.go langdao-ec-gb.dict.dz langdao-ec-gb.idx langdao-ec-gb.ifo
 	a, err := Asset("langdao-ec-gb.ifo")
 	if err != nil {
 		log.Fatal(err)
@@ -16,7 +20,6 @@ func main() {
 		log.Fatal(err)
 	}
 	c, err := Asset("langdao-ec-gb.dict.dz")
-
 
 	r1 := bytes.NewReader(a)
 	r2 := bytes.NewReader(b)
