b6fa1825757df8e4bdb9cff1de599586730e1c9c
rongyi <sqrongyi@163.com>
2019-03-07 17:17:48 +0800

    Update gen sql script, make it accept command args

--- cmd/tosql/main.go
+++ cmd/tosql/main.go
@@ -1,22 +1,63 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/rongyi/stardict/pkg/parser"
 	"github.com/rongyi/stardict/pkg/sql"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
 
-const (
-	dbName = "langdao.db"
+var (
+	dbName        string
+	dictionaryDir string
+
+	ifoFile  string
+	idxFile  string
+	dictFile string
 )
+
+func bindDictFile() {
+	files, err := ioutil.ReadDir(dictionaryDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+
+		if strings.HasSuffix(f.Name(), ".ifo") {
+			ifoFile = filepath.Join(dictionaryDir, f.Name())
+		} else if strings.HasSuffix(f.Name(), ".idx") {
+			idxFile = filepath.Join(dictionaryDir, f.Name())
+		} else if strings.HasSuffix(f.Name(), "dict.dz") {
+			dictFile = filepath.Join(dictionaryDir, f.Name())
+		}
+	}
+}
+
+func init() {
+	flag.StringVar(&dbName, "db", "", "specify db name")
+	flag.StringVar(&dictionaryDir, "dict", "", "specify dictionary dir")
+	flag.Parse()
+}
 
 func main() {
+	if dbName == "" || dictionaryDir == "" {
+		log.Fatal("need db name and dictionary dir")
+	}
+	bindDictFile()
 	// assume the existed file is a db we created
 	// create db if needed
 	if _, err := os.Stat(dbName); err != nil {
-		if err := sql.CreateLangdaoTable("langdao.db"); err != nil {
+		if err := sql.CreateLangdaoTable(dbName); err != nil {
 			log.Fatal(err)
 		}
 	}
@@ -28,17 +69,17 @@ func main() {
 	defer db.Close()
 
 	// create dictionary
-	f1, err := os.Open("../../pkg/parser/testdata/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.ifo")
+	f1, err := os.Open(ifoFile)
 	if err != nil {
 		log.Fatalf("%s\n", "fail to create new dictionary")
 	}
 	defer f1.Close()
-	f2, err := os.Open("../../pkg/parser/testdata/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.idx")
+	f2, err := os.Open(idxFile)
 	if err != nil {
 		log.Fatalf("%s\n", "fail to create new dictionary")
 	}
 	defer f2.Close()
-	f3, err := os.Open("../../pkg/parser/testdata/stardict-langdao-ec-gb-2.4.2/langdao-ec-gb.dict.dz")
+	f3, err := os.Open(dictFile)
 	if err != nil {
 		log.Fatalf("%s\n", "fail to create new dictionary")
 	}
