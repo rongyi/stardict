b8ab0bc8e5fc8a0450b9fcc5915f833b46586897
rongyi <sqrongyi@163.com>
2019-03-20 17:43:01 +0800

    Update dbversion

---
+++ cmd/dbversion/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/rongyi/stardict/pkg/tui"
+)
+
+func main() {
+	ea := &tui.EngineAttribute{
+		DefaultQuery: "",
+		Monochrome:   false,
+	}
+
+	e, err := tui.NewEngine("./langdao-ec.db", ea)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(run(e))
+
+}
+
+func run(e tui.EngineInterface) int {
+	result := e.Run()
+	if result.GetError() != nil {
+		return 2
+	}
+	fmt.Printf("%s\n", result.GetQueryString())
+	fmt.Printf("%s\n", result.GetContent())
+	return 0
+}
